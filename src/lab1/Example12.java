e30067ba7fbaf8b0386ce71bde1b661fc67b6928
TorhovKonstantin <tokhov19@mail.ru>
2023-01-10 22:40:40 +0500

    initial commit

---
+++ src/lab1/Example12.java
@@ -0,0 +1,17 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Example12 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите свой возраст");
+        int year = in.nextInt();
+        int years;
+        int a = 2023;
+        years = a - year;
+        System.out.println("Вы родились в " + years +" году.");
+    }
+
+}
